b7a2af4e97bcab9a206d86609933d9ff8b150770
dev-rb <43100342+dev-rb@users.noreply.github.com>
2020-12-28 22:10:11 -0500

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
   const dispatch = useDispatch();
 
   React.useEffect(() => {
-    dispatch(getCourses('https://hunter-cs-syllabi.herokuapp.com/classes'))
+    dispatch(getCourses('https://hunter-cs-syllabi-a.herokuapp.com/classes'))
   }, [dispatch])
 
   return (
